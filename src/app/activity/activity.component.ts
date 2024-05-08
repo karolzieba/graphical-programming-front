40dd818654a1ea1d1b6dba800376a5a2cfc956ce
Karol Zięba <kz516749@gmail.com>
2024-05-08 20:01:24 +0200

    Added conditions.

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -23,14 +23,18 @@ declare var mermaid: any;
 export class ActivityComponent {
   idElement: number = 0;
   idConnection: number = 0;
-
+  numberOfConditions: number = 0;
   elements: { id: number, type: string, label: string, visible: boolean }[] = [];
   connections: { id: number, src: number, trg: number }[] = [];
-
   actionFormText: string = "";
-
+  connectionCommentFormText: string = "";
   displayConnectionDialog: boolean = false;
+  displayConnectionCommentDialog: boolean = false;
   displayActionDialog: boolean = false;
+  displayElementsDialog: boolean = false;
+  displayConnectionsDialog: boolean = false;
+  connectionRemoveOnClick: boolean = false;
+  connectionCommentTarget: number = -1;
 
   ngAfterViewInit(): void {
     mermaid.initialize({
@@ -64,18 +68,23 @@ export class ActivityComponent {
     let code = "stateDiagram-v2";
     if (this.connections.length != 0) {
       this.elements.forEach(element => {
-        if (element.label.includes("<<choice>>")) code += " " + element.label;
+        if (element.label.includes("<<choice>>")) code += " " + element.label + "\n";
       })
       this.connections.forEach(connection => {
         let src = this.elements[connection.src];
         let trg = this.elements[connection.trg];
         if (src.visible && trg.visible) {
-          code += " " + src.label + "-->" + trg.label;
+          let labelParts = src.label.split(" : ");
+          if (labelParts.length == 2) {
+            code += " " + labelParts[0] + "-->" + trg.label + "\n";
+          } else {
+            code += " " + src.label + "-->" + trg.label + "\n";
+          }
         }
       });
     } else {
       this.elements.forEach(element => {
-        code += " " + element.label;
+        code += " " + element.label + "\n";
       });
     }
     console.log(code);
@@ -99,7 +108,22 @@ export class ActivityComponent {
     this.displayActionDialog = !this.displayActionDialog;
   }
 
+  showElementsDialog() {
+    if (this.elements.length != 0) {
+      this.displayElementsDialog = !this.displayElementsDialog;
+    }
+  }
+
+  showConnectionsDialog(removeOnClick: boolean) {
+    this.connectionRemoveOnClick = removeOnClick;
+    if (this.connections.length != 0) {
+      this.displayConnectionsDialog = !this.displayConnectionsDialog;
+    }
+    if (this.displayConnectionCommentDialog) this.displayConnectionCommentDialog = !this.displayConnectionCommentDialog;
+  }
+
   appendFirstElement() {
+    if (this.elements.some(element => element.type === "first")) return;
     this.elements.push({ id: this.idElement, type: "first", label: "[*]", visible: true });
     this.idElement++;
     this.refreshDiagramView();
@@ -113,14 +137,20 @@ export class ActivityComponent {
   }
 
   appendConditionElement() {
-    this.elements.push({ id: this.idElement, type: "condition", label: "state if_state <<choice>>", visible: false});
+    let conditionName: string = "if_state";
+    if (this.numberOfConditions !== 0) {
+      conditionName += this.numberOfConditions;
+    }
+    this.numberOfConditions++;
+    this.elements.push({ id: this.idElement, type: "condition", label: "state " + conditionName + " <<choice>>", visible: false });
     this.idElement++;
-    this.elements.push({ id: this.idElement, type: "condition", label: "if_state", visible: true});
+    this.elements.push({ id: this.idElement, type: "condition", label: conditionName, visible: true });
     this.idElement++;
     this.refreshDiagramView();
   }
 
   appendLastElement() {
+    if (this.elements.some(element => element.type === "last")) return;
     this.elements.push({ id: this.idElement, type: "last", label: "[*]", visible: true });
     this.idElement++;
     this.refreshDiagramView();
@@ -132,9 +162,35 @@ export class ActivityComponent {
     this.refreshDiagramView();
   }
 
+  appendConnectionCommentElement(connection: any) {
+    if (typeof connection === "string") {
+      this.elements[this.connectionCommentTarget].label += " : " + connection;
+      this.refreshDiagramView();
+      this.connectionCommentFormText = "";
+      this.connectionCommentTarget = -1;
+    } else {
+      this.displayConnectionCommentDialog = true;
+      this.connectionCommentTarget = connection.trg;
+    }
+  }
+
   clearElements() {
     this.elements = [];
     this.connections = [];
+    this.displayConnectionDialog = false;
+    this.refreshDiagramView();
+  }
+
+  removeElement(element: any) {
+    this.connections = this.connections.filter(connection => connection.src === element.id || connection.trg === element.id);
+    this.elements = this.elements.filter(elementFromArray => elementFromArray.id != element.id);
+    this.displayConnectionDialog = false;
+    this.refreshDiagramView();
+  }
+
+  removeConnection(connection: any) {
+    this.elements[connection.trg].label = this.elements[connection.trg].label.split(" : ")[0];
+    this.connections = this.connections.filter(connectionFromArray => connectionFromArray.id !== connection.id);
     this.refreshDiagramView();
   }
 }
