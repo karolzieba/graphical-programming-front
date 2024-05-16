1e4ea19ce293a63b75d6140e6da4f1dfd7132725
ziomidlo <kziomek33@gmail.com>
2024-05-16 13:21:25 +0200

    add first implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { ActivityComponent } from './activity/activity.component';
+import { SequenceDiagramComponent } from './sequence-diagram/sequence-diagram.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ActivityComponent, MatButtonToggleModule],
+  imports: [RouterOutlet, ActivityComponent, MatButtonToggleModule, SequenceDiagramComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedDiagram:string = 'sequence';
+}
